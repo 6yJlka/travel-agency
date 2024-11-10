3e706f0973fa12b045d655f10d04ca315786bd38
Донских Дмитрий Георгиевич <donskikhdg@student.bmstu.ru>
2024-11-10 21:39:05 +0300

    keep doing front

--- TourAgency/tour-management/frontend/src/pages/Home.jsx
+++ TourAgency/tour-management/frontend/src/pages/Home.jsx
@@ -8,6 +8,8 @@ import heroVideo from '../assets/images/hero-video.mp4'
 import Subtitle from './../shared/Subtitle';
 
 import SearchBar from "../shared/SearchBar"
+import ServiceList from "../services/ServiceList"
+import FeaturedTourList from "../components/Featured-tours/FeaturedTourList"
 
 const Home = () => {
     return <>
@@ -58,9 +60,23 @@ const Home = () => {
                     <h5 className="services__subtitle">What we server</h5>
                     <h2 className="services__title">We offer our best services</h2>
                 </Col>
+                <ServiceList/>
             </Row>
         </Container>
     </section>
+    {/*============= featured tour section start ===============*/}
+    <section>
+        <Container>
+            <Row>
+                <Col lg='12' className="mb-5">
+                    <Subtitle subtitle={'Explore'}/>
+                    <h2 className="featured__tour-title">Our featured tours</h2>
+                </Col>
+                <FeaturedTourList/>
+            </Row>
+        </Container>
+    </section>
+    {/*============= featured tour section end ===============*/}
     </>
 }
 
